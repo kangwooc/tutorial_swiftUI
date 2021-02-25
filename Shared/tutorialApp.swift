0621b23139f37f849d9672dc09eee92cc4bbe23e
Kangwoo Choi <troy@akaai.kr>
2021-02-25 10:35:48 +0900

    Initial Commit

---
+++ Shared/tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorialApp.swift
+//  Shared
+//
+//  Created by Kangwoo Choi on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
